aa70f43f4fe4b8cf658a2e52a366069cac230a01
ealton <longjdai@gmail.com>
2017-12-05 22:57:27 +0800

    update

--- Appv5/Controller/StripePaymentController.class.php
+++ Appv5/Controller/StripePaymentController.class.php
@@ -45,7 +45,7 @@ class StripePaymentController extends BaseController {
 		}
 		
 		$this->assign('data', $order);
-		$this->display('./Application/App/View/Order/orderpay.html');
+		$this->display('./Application/Appv5/View/Order/orderpay.html');
 	}
 	
 	/**
@@ -170,4 +170,110 @@ class StripePaymentController extends BaseController {
 			exit();
 		}
 	}
+    
+    
+    /**
+	 * Pay order Test
+	 */
+	/*
+    public function order_pay_test () {
+	
+		//$stripeToken = $this->get_param('post.stripe_token');
+		$orderID = $this->get_param('get.order_id');
+		$token = $this->get_param('post.token');
+		$credit_card_number = $this->get_param('post.credit_card_number');
+		$security_code = $this->get_param('post.security_code');
+		$expiration_time = $this->get_param('post.expiration_time');
+		
+	
+        OrderController::CheckCouponStatus($orderID);
+		$order = M('order')
+            ->where("`id` = $orderID")
+            ->find();
+        
+        
+	
+		$re_data = array();
+			
+		if(!$orderID) {
+			$this->return_error('Missing order id');
+		}
+		
+		M('order')
+            ->where("`id` = $orderID")
+            ->save([
+                'credit_card_number'=>$credit_card_number, 
+                'security_code'=>$security_code, 
+                'expiration_time'=>$expiration_time]);
+        
+        
+
+				
+			
+		$this->targetRegionID = $order['dregion_id'];
+		
+        // Update order data
+		M('order')
+            ->where("`id` = $orderID")
+            ->save([
+                'is_payment'=>1, 'status'=>2, 
+                'credit_card_number'=>$order['id']]);
+        
+        // Update order sub data
+		M('order_sub')
+            ->where("`order_id` = $orderID")
+            ->save(['status'=>2]);
+        
+        // Update coupon data
+        $couponSN = $order['coupon_sn'];
+        if ($couponSN == 'First Order') {
+            M('user')
+                ->where("`id` = " . $order['user_id'])
+                ->save(['has_made_first_order' => 1]);
+        } else {
+            $couponSNDetail = M('coupon_sn')
+                ->where("`sn` = '$couponSN'")
+                ->find();
+            if ($couponSNDetail['reusable'] == 0) {
+                M('coupon_sn')
+                    ->where("`sn` = '$couponSN'")
+                    ->save(['status' => 1]);
+            }
+        }
+        
+        // Send emails
+		$sub_orders = M('order_sub')
+            ->where("`order_id` = $orderID")
+            ->field('`id`, `dregion_id`')
+            ->select();
+        $sub_order_numbers = "";
+		if(array_filter($sub_orders)) {
+			foreach ($sub_orders as $sub_order) {
+				M('order_goods')
+                    ->where('`sub_order_id`='.$sub_order['id'])
+                    ->save(['status'=>2]);
+                if ($sub_order_numbers == "") {
+                    $sub_order_numbers = $sub_order['order_number'];
+                } else {
+                    $sub_order_numbers = $sub_order_numbers . ", " . $sub_order['order_number'];
+                }
+			}
+		}
+        
+        
+        
+		$this->user_email_set($order, $sub_orders[0]['dregion_id'], $sub_order_numbers);
+		$this->platform_email_set($order);
+		$this->merchant_email_set($order);
+		
+		$re_data['code'] = 1;
+
+        
+	
+		//$re_data['message'] = 'haha';
+		echo json_encode($re_data);
+		exit();
+		
+	}
+    */
 }
