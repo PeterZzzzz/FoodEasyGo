5d49de2b8b53e9c5f7bd08813abae56891559a9a
“Yang <yang.zheng.peter@gmail.com>
2019-02-14 14:37:37 -0500

    Version 3.0.1
    -上传了所需要的app icon和lunch image

--- FoodEasyGoIOS_Unity2017/Assets/__Scripts/Config/Config.cs
+++ FoodEasyGoIOS_Unity2017/Assets/__Scripts/Config/Config.cs
@@ -7,7 +7,7 @@ public static class Config
     // Common
     public static Language currentLanguage = Language.chinese;
     public static float panelSlideTime = 0.3f;
-    public static string versionNumberString = "Version 3.0.0";
+    public static string versionNumberString = "Version 3.0.1";
 #if UNITY_EDITOR
     public static bool isDebug = true;
 #else
